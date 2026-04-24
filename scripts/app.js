49ef30b140553b0d44a8a57db6ca44d9846d40f2
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-04-24 10:57:45 +0530

    feat: implement 5-day iterative forecast grid with Intl date formatting

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const cityInput = document.querySelector("#city-input");
 const currentWeather = document.querySelector("#current-weather");
+const weatherForecastSection = document.querySelector("#weather-forecast");
 
 function renderCurrentWeather(data) {
   currentWeather.replaceChildren();
@@ -22,6 +23,46 @@ function renderCurrentWeather(data) {
   currentWeather.append(h2, p, img);
 }
 
+function filterForecastData(forecastList) {
+  const forecastFilteredList = forecastList.filter((forecast) =>
+    forecast.dt_txt.includes("12:00:00"),
+  );
+  return forecastFilteredList;
+}
+
+function renderForecast(forecastList) {
+  weatherForecastSection.replaceChildren();
+
+  forecastList.forEach((forecast) => {
+    const article = document.createElement("article");
+    article.classList.add("forecast-card");
+
+    const { temp } = forecast.main;
+    const { icon, description } = forecast.weather[0];
+
+    const p = document.createElement("p");
+    p.textContent = `${temp}`;
+
+    const img = document.createElement("img");
+    img.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    img.alt = description;
+
+    article.append(p, img);
+
+    const dateObj = new Date(forecast.dt * 1000);
+    const dayName = new Intl.DateTimeFormat("en-US", {
+      weekday: "long",
+    }).format(dateObj);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = dayName;
+
+    article.prepend(h3);
+
+    weatherForecastSection.appendChild(article);
+  });
+}
+
 async function fetchWeather(city) {
   try {
     const api = `https://weather-proxy.pavanganeshbutha.workers.dev/?city=${city}`;
@@ -31,7 +72,9 @@ async function fetchWeather(city) {
     }
     const data = await response.json();
     localStorage.setItem("lastSearchedCity", city);
-    renderCurrentWeather(data);
+    renderCurrentWeather(data.current);
+    const dailyForecast = filterForecastData(data.forecast.list);
+    renderForecast(dailyForecast);
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
